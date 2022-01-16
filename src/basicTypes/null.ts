74be1d7bd946014fe679ca963487805b9f448c21
신형기 <tkaqkeldk@hanatour.com>
2022-01-16 18:41:17 +0900

    undefined , null 추가

---
+++ src/basicTypes/null.ts
@@ -0,0 +1,18 @@
+// let MyName: string = null;
+
+// let u: undefined = null;
+
+//  let v: void = void;
+
+let v: void = undefined; // null X 
+
+// let union: string = null;
+
+// 합집합 같은 개념.
+let union: string | null = null;
+
+union = "Mark";
+
+/**
+ * runTime 에서 null=> object | undefined => undefined
+ */
