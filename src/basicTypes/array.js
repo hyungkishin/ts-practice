75f376897c70eea388264b31330bf075ec8d199b
신형기 <tkaqkeldk@hanatour.com>
2022-01-16 18:55:36 +0900

    tuple update

---
+++ src/basicTypes/array.js
@@ -0,0 +1,4 @@
+"use strict";
+// let numArr: number[] = [1, 2, 3, 4, 5]; // 선호
+let numArr = [1, 2, 3, "4", 5]; // 문자도 가능하게. union Type 으로 작성 
+let numberArr = [1, 2, 3, 4, 5]; // jsx tsx 에서 충돌날 수 있어 사용을 자제.
---
+++ src/basicTypes/array.ts
@@ -0,0 +1,4 @@
+// let numArr: number[] = [1, 2, 3, 4, 5]; // 선호
+let numArr: (number | string)[] = [1, 2, 3, "4", 5]; // 문자도 가능하게. union Type 으로 작성 
+
+let numberArr: Array<number> = [1, 2, 3, 4, 5] // jsx tsx 에서 충돌날 수 있어 사용을 자제.
